22fde2deace18719b558666b22a27b2ddf3fd170
HoyeongJeon <hoyeong0302@gmail.com>
2025-02-10 01:17:14 +0900

    쿠폰 개수 검사를 redis로 변경

---
+++ couponwithlock/src/main/kotlin/blog/couponwithlock/coupon/domain/CouponCountRepository.kt
@@ -0,0 +1,15 @@
+package blog.couponwithlock.coupon.domain
+
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.stereotype.Repository
+
+@Repository
+class CouponCountRepository(
+    private val redisTemplate: RedisTemplate<String, String>
+) {
+    fun increment(): Long? {
+        return redisTemplate
+            .opsForValue()
+            .increment("couponCount")
+    }
+}
