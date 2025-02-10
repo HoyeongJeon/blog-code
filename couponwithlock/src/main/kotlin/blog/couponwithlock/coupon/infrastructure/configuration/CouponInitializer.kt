8e642c4a32da5c695a983d95ea3a670224d96b12
HoyeongJeon <hoyeong0302@gmail.com>
2025-02-11 01:36:26 +0900

    README 추가

--- couponwithlock/src/main/kotlin/blog/couponwithlock/coupon/infrastructure/configuration/CouponInitializer.kt
+++ couponwithlock/src/main/kotlin/blog/couponwithlock/coupon/infrastructure/configuration/CouponInitializer.kt
@@ -3,7 +3,6 @@ package blog.couponwithlock.coupon.infrastructure.configuration
 import blog.couponwithlock.coupon.domain.Coupon
 import blog.couponwithlock.coupon.domain.CouponRepository
 import jakarta.annotation.PostConstruct
-import jakarta.transaction.Transactional
 import org.springframework.context.annotation.Configuration
 
 @Configuration
@@ -11,7 +10,6 @@ class CouponInitializer(
     private val couponRepository: CouponRepository
 ) {
     @PostConstruct
-    @Transactional
     fun initCoupon() {
         for (i in 1..100) {
             couponRepository.save(Coupon(code = CouponIdGenerator.codeGenerate()))
