fb93c52f66a70f87e1c01be5002a25c8743f1938
HoyeongJeon <hoyeong0302@gmail.com>
2025-02-11 14:32:29 +0900

    분산락을 통한 동시성 제어 구현

--- couponwithlock/src/main/kotlin/blog/couponwithlock/coupon/application/CouponService.kt
+++ couponwithlock/src/main/kotlin/blog/couponwithlock/coupon/application/CouponService.kt
@@ -3,15 +3,13 @@ package blog.couponwithlock.coupon.application
 import blog.couponwithlock.coupon.domain.CouponIssueService
 import blog.couponwithlock.coupon.presentation.dto.request.CouponIssueRequest
 import blog.couponwithlock.coupon.presentation.dto.response.CouponIssueResponse
-import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 
 @Service
 class CouponService(
     private val couponIssueService: CouponIssueService
 ) {
-    @Transactional
     fun issueCoupon(couponIssueRequest: CouponIssueRequest): CouponIssueResponse {
-        return CouponIssueResponse(couponIssueService.issueCoupon(couponIssueRequest.userId).id!!)
+        return CouponIssueResponse(couponIssueService.issueCoupon(couponIssueRequest.userId)?.id!!)
     }
 }
--- couponwithlock/src/main/kotlin/blog/couponwithlock/coupon/domain/CouponIssueService.kt
+++ couponwithlock/src/main/kotlin/blog/couponwithlock/coupon/domain/CouponIssueService.kt
@@ -1,20 +1,48 @@
 package blog.couponwithlock.coupon.domain
 
+import blog.couponwithlock.core.exception.error.GlobalErrorCode
 import blog.couponwithlock.coupon.exception.CouponErrorCode
+import org.redisson.api.RedissonClient
 import org.springframework.stereotype.Service
+import org.springframework.transaction.PlatformTransactionManager
+import org.springframework.transaction.support.TransactionTemplate
+import java.util.concurrent.TimeUnit
 
 @Service
 class CouponIssueService(
     private val couponRepository: CouponRepository,
-    private val couponCountRepository: CouponCountRepository
-    ) {
-    fun issueCoupon(userId: Long): Coupon {
-        couponRepository.findByUserId(userId)?.let {
-            return it
+    private val couponCountRepository: CouponCountRepository,
+    private val redissonClient: RedissonClient,
+    private val transactionManager: PlatformTransactionManager
+) {
+    fun issueCoupon(userId: Long): Coupon? {
+
+        val lock = redissonClient.getLock("COUPON_ISSUE_DISTRIBUTED_LOCK")
+
+        if (!lock.tryLock(2, 15, TimeUnit.SECONDS)) {
+            throw GlobalErrorCode.FAILED_TO_ACQUIRE_LOCK.toException()
         }
-        if (couponCountRepository.increment()!! > 100) {
-            throw CouponErrorCode.ALL_COUPON_ISSUED.toException()
+
+        try {
+            return TransactionTemplate(transactionManager).execute {
+                couponRepository.findByUserId(userId)?.let {
+                    return@execute it
+                }
+
+                val couponCount = couponCountRepository.count() ?: 0
+
+                if (couponCount >= 100) {
+                    throw CouponErrorCode.ALL_COUPON_ISSUED.toException()
+                }
+
+                couponCountRepository.increment()
+
+                return@execute couponRepository.save(Coupon.create(userId))
+            }
+        } finally {
+            if (lock.isHeldByCurrentThread) {
+                lock.unlock()
+            }
         }
-        return couponRepository.save(Coupon.create(userId))
     }
 }
